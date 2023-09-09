1551439a3106ed9b6dfd37f2c9efd903c4209ba3
Gregory Macat <gregorymacat@gmail.com>
2023-09-09 00:59:48 -0500

    Node command changes (#17)
    * Updated configs for prod and dev building
    
    * Should no longer need overall config webpack file
    
    * Fixed building and start commands

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,31 @@
 const path = require('path');
+/*
+ const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+ module.exports = {
+   entry: {
+     app: './src/index.js',
+   },
+   plugins: [
+     new HtmlWebpackPlugin({
+       title: 'Production',
+     }),
+   ],
+   output: {
+     filename: '[name].bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+     clean: true,
+   },
+ };
+*/
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  devtool: 'inline-source-map',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
+    // clean: true,
   },
   module: {
     rules: [
